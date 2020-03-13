d5d096c131250da4d88c7d84c7c2473605c1bca7
Lee Edwards <lee@root.vc>
2020-03-13 02:28:03 -0700

    Initial commit

---
+++ build/precache-manifest.83ce403942ffc634e7ac60c45c120dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6414524013cfeda0a9f73c963bff22",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/index.html"
+  },
+  {
+    "revision": "e941bc92384dec8da6ce",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c6f03d80834009d8b1bf",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/static/js/2.30308810.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/static/js/2.30308810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e941bc92384dec8da6ce",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/static/js/main.eda2b3ed.chunk.js"
+  },
+  {
+    "revision": "5f45aca13e8ab7c4daaf",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/static/js/runtime-main.e159580b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "DARK_STATIC_ASSETS_BASE_URL/static/media/logo.5d5d9eef.svg"
+  }
+]);
